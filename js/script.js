df77b7e117675d07e8562895cd3e185f0a1b594c
Jessé Júnior <jessejuniormaciel@hotmail.com>
2022-05-18 20:28:34 -0300

    fetch-animais refatorado

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetchAnimais.js';
+import fetchAnimais from './modules/fetchAnimais.js';
 import initFetchBitcoin from './modules/fetchBitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 
@@ -33,6 +33,6 @@ tooltip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
 initFetchBitcoin();
 initAnimacaoScroll();
