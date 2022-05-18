5647695f8401b51a23852e2ff597c319bb127fd0
Jessé Júnior <jessejuniormaciel@hotmail.com>
2022-05-18 20:52:13 -0300

    fetch-bitcoin refatorado

--- js/modules/fetchBitcoin.js
+++ js/modules/fetchBitcoin.js
@@ -1,9 +1,9 @@
-export default function initFetchBitcoin() {
-  async function fetchBitcoin(url) {
+export default function initFetchBitcoin(url, target) {
+  async function fetchBitcoin() {
     try {
       const bitcoinResponse = await fetch(url);
       const bitcoinJSON = await bitcoinResponse.json();
-      const btnPreco = document.querySelector('.btc-preco');
+      const btnPreco = document.querySelector(target);
       // btnPreco.innerText = Number.prototype.toFixed.call((1000/bitcoinJSON.BRL.sell), 4);
       btnPreco.innerText = (1000 / bitcoinJSON.BRL.sell).toFixed(4);
     } catch (error) {
@@ -11,5 +11,5 @@ export default function initFetchBitcoin() {
     }
   }
 
-  fetchBitcoin('https://blockchain.info/ticker');
+  fetchBitcoin(url);
 }
